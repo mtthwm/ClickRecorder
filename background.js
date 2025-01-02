bc7ba8e7abbe099571ae451a06997cc9adfafc25
Matthew Morales <mtthwmrls9@gmail.com>
2025-01-01 22:57:43 -0700

    Event logging finished

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
 let recording = false;
+let clicks = [];
+
+function handleRecordingStart () {
+
+}
+
+function handleRecordingEnd () {
+    clicks = [];
+}
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {    
     if (message.action === "toggleRecording") {
         recording = !recording;
+        if (recording) {
+            handleRecordingStart();
+        } else {
+            handleRecordingEnd();
+        }
     } else if (message.action === "requestRecordingStatus") {
-        console.log(message);
         sendResponse({reply: recording});
+    } else if (message.action === "clickAction") {
+        if (recording) {
+            const clickAction = message.extraInfo.action;
+            clicks.push(clickAction)
+        }
     }
 
     return false;
--- content.js
+++ content.js
@@ -1,4 +1,17 @@
+function ClickAction (elementType, elementId, elementClass) {
+    return {
+        elementClass: elementClass,
+        elementId: elementId,
+        elementType: elementType,
+    }
+}
+
 document.addEventListener("click", event => {
-    console.log(event.target);
+    let action = ClickAction(
+        event.target.nodeName.toLowerCase(),
+        event.target.id,
+        event.target.classList
+    )
+    chrome.runtime.sendMessage({ action: "clickAction", name: "Popup", extraInfo: {action: action}});
 });
 
